bc9081620acab186a7176c99030bdf78dbce2289
yunseo0219 <chldbstj021902@gmail.com>
2026-01-10 20:01:00 +0900

    Initial commit from Specify template

---
+++ src/main/java/hello/tradexserver/TradexServerApplication.java
@@ -0,0 +1,13 @@
+package hello.tradexserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradexServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradexServerApplication.class, args);
+    }
+
+}
