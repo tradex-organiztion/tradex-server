bab9deac9cb4bbb983b8ddaa2a8ebd40d4571f64
yunseo0219 <chldbstj021902@gmail.com>
2026-02-28 16:13:57 +0900

    add: cors PATCH method 추가

--- src/main/java/hello/tradexserver/config/SecurityConfig.java
+++ src/main/java/hello/tradexserver/config/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
