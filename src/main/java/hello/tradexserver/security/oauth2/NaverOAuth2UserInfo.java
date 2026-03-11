ef4375a4a490f5a5efffc3a30ccd1a021b27cb53
sehyun <sese2204@naver.com>
2026-03-11 19:21:52 +0900

    feature: 네이버 소셜 로그인 구현

---
+++ src/main/java/hello/tradexserver/security/oauth2/NaverOAuth2UserInfo.java
@@ -0,0 +1,42 @@
+package hello.tradexserver.security.oauth2;
+
+import java.util.Map;
+
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+
+    public NaverOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getId() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        if (response == null) return null;
+        return (String) response.get("id");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getEmail() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        if (response == null) return null;
+        return (String) response.get("email");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getName() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        if (response == null) return null;
+        return (String) response.get("name");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getImageUrl() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        if (response == null) return null;
+        return (String) response.get("profile_image");
+    }
+}
